12bd1f026a2c74b8a704761fbdcc59233bd05237
shantanuparte <shantanuparte23@gmail.com>
2026-09-17 22:13:52 +0530

    added api's adn other things

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	weathers "GeoShan/weather"
 	"fmt"
 	"os"
-
-	"GeoShan/weather"
+	"strings"
 
 	"charm.land/lipgloss/v2"
 )
 
 func main() {
-	fmt.Println("This is start of the project")
 	place := os.Args[1]
-
+	upper_place := strings.ToUpper(place)
 	style := lipgloss.NewStyle().
 		Bold(true).
+		Width(60).
 		Foreground(lipgloss.Color("#FAFAFA")).
 		Background(lipgloss.Color("#7D56F4")).
-		Padding(1, 2)
+		Padding(2, 4).
+		Align(lipgloss.Center)
 
-	fmt.Println(style.Render("Hello, my first lipgloss project"))
-
-	weathers.GetWeatherInfo(place)
+	fmt.Println(style.Render(upper_place))
+	
+	var something *weathers.ApiInfo
+	
+	something, err := weathers.GetWeatherInfo(upper_place)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	
+	fmt.Println(something)
+	
 }
--- weather/client.go
+++ weather/client.go
@@ -1,24 +1,44 @@
 package weathers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 )
 
-func GetWeatherInfo(place string) {
-
-	fmt.Println("This funciton is running")
+func GetWeatherInfo(place string) (*ApiInfo, error) {
 	client := &http.Client{
-		Timeout: time.Second*5,
+		Timeout: time.Second * 5,
 	}
-	url := fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json",place)
+
+	url := buildUrl(place)
 
 	resp, err := client.Get(url)
-	if err != nil{
-		fmt.Println("Error in getting reponse")
-		return
+	if err != nil {
+		return nil, fmt.Errorf("Error in getting response")
 	}
 
-	
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("api error: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	//Unmarshalling
+	var WeathInfo ApiInfo
+
+	err = json.NewDecoder(resp.Body).Decode(&WeathInfo)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode")
+	}
+
+	return &WeathInfo, nil
+
+}
+
+
+func buildUrl(place string) string{
+	encodePlace := url.QueryEscape(place)
+	return fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json", encodePlace)
 }
