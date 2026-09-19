e1e0d96ff5a260a42d12ad458cc2b1a3ecaa026f
shantanuparte <shantanuparte23@gmail.com>
2026-09-19 22:03:32 +0530

    Restructured main.go and trying different styling

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/charmbracelet/x/windows v0.2.2 // indirect
 	github.com/clipperhouse/displaywidth v0.11.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
 	github.com/lucasb-eyer/go-colorful v1.4.1 // indirect
 	github.com/mattn/go-runewidth v0.0.24 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
--- main.go
+++ main.go
@@ -4,68 +4,53 @@ import (
 	weathers "GeoShan/weather"
 	"fmt"
 	"os"
+	"github.com/common-nighthawk/go-figure"
 	"charm.land/lipgloss/v2"
 )
 
+var style = lipgloss.NewStyle().
+	Bold(true).
+	Width(60).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4")).
+	Padding(2, 4).
+	Align(lipgloss.Center)
+
 func main() {
 
+	var location *weathers.Location
+	var weather *weathers.ApiInfo
+	var err error
+
 	if len(os.Args) > 1 {
-		place := os.Args[1]
-
-		var something *weathers.Location
-
-		something, err := weathers.GetLocatoinCoordinates(place)
-		if err != nil {
-
-			fmt.Printf("Error: %v\n", err)
-			return
-		}
-
-		fmt.Println(something)
-
-		var weather_info *weathers.ApiInfo
-
-		weather_info, err = weathers.GetWeatherInfo(something)
-		if err != nil {
-			fmt.Printf("Error: %v", err)
-			return
-		}
-
-		fmt.Printf("Country: %v\nCity: %v\nLatitude: %v\nLongitude: %v\n", something.Country, something.Name, something.Lat, something.Lon)
-
-		fmt.Println(weather_info)
-		fmt.Println(weathers.WeahterCodeConvertion(weather_info.Current.WeatherInfo))
-
+		location, err = weathers.GetLocatoinCoordinates(os.Args[1])
 	} else {
-		var coordinates *weathers.Location
-
-		coordinates, err := weathers.GetLocatoinCoordinatesWithoutArguments()
-		if err != nil {
-			fmt.Printf("%v", err)
-			return
-		}
-
-		var weather_info_without_args *weathers.ApiInfo
-
-		weather_info_without_args, err = weathers.GetWeatherInfo(coordinates)
-		if err != nil {
-			fmt.Printf("%v\n", err)
-		}
-
-		fmt.Printf("Country: %v\nCity: %v\nLatitude: %v\nLongitude: %v\n", coordinates.Country, coordinates.Name, coordinates.Lat, coordinates.Lon)
-		fmt.Println(weather_info_without_args)
-
+		location, err = weathers.GetLocatoinCoordinatesWithoutArguments()
 	}
 
-	style := lipgloss.NewStyle().
-		Bold(true).
-		Width(60).
-		Foreground(lipgloss.Color("#FAFAFA")).
-		Background(lipgloss.Color("#7D56F4")).
-		Padding(2, 4).
-		Align(lipgloss.Center)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
 
-	fmt.Println(style.Render("Currenty working"))
-	fmt.Print("hii")
+	weather, err = weathers.GetWeatherInfo(location)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	
+	Render(location,weather)
+	
+	
+}
+
+func Render(location *weathers.Location, weather *weathers.ApiInfo)(){
+	
+	fig := figure.NewFigure(location.Name, "doom",true)
+	fig.Print()
+	fmt.Println(style.Render(location.Name))
+	fmt.Println(weather)
+	fmt.Println(location)
 
 }
